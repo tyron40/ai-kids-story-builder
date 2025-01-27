8382a694af0726690cf06ff0013fd73eca308e95
Insider32 <forsakenxz@gmail.com>
2025-01-27 01:14:08 +0100

    feat: ability to take photo as story seed image

--- app/create-story/_components/ImageInput.tsx
+++ app/create-story/_components/ImageInput.tsx
@@ -1,9 +1,16 @@
 import { Input } from "@nextui-org/input";
-import { Button, Image } from "@nextui-org/react";
+import { Button, Image, useDisclosure } from "@nextui-org/react";
 import React from "react";
+import TakePhoto from "./TakePhoto";
 
 function ImageInput({ userSelection }: any) {
   const [image, setImage] = React.useState<string | null>(null);
+  const {
+    isOpen: isTakePhotoOpen,
+    onOpen: onTakePhotoOpen,
+    onClose: onTakePhotoClose,
+    onOpenChange: onTakePhotoOpenChange
+  } = useDisclosure()
 
   const onFilePick: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     const file = e.target.files?.[0];
@@ -16,6 +23,14 @@ function ImageInput({ userSelection }: any) {
       });
     }
   };
+
+  const onPhotoPick = (image: string) => {
+    setImage(image)
+    userSelection({
+      fieldName: "storyImage",
+      fieldValue: image,
+    });
+  }
 
   const onFileRemove = () => {
     setImage(null);
@@ -39,9 +54,12 @@ function ImageInput({ userSelection }: any) {
           className="max-w-md"
           onChange={onFilePick}
         />
+        <Button onPress={onTakePhotoOpen}>
+          Take Photo
+        </Button>
         {image && (
-          <div className="relative flex justify-center items-center gap-2">
-            <Image src={image} height={180} />
+          <div className="relative flex justify-center items-center gap-2 max-h-[180px]">
+            <Image src={image} className="max-h-[180px]"/>
             <Button
               color="primary"
               onPress={onFileRemove}
@@ -52,6 +70,12 @@ function ImageInput({ userSelection }: any) {
           </div>
         )}
       </div>
+      <TakePhoto
+        isOpen={isTakePhotoOpen}
+        onClose={onTakePhotoClose}
+        onOpenChange={onTakePhotoOpenChange}
+        onPhotoPick={onPhotoPick}
+      />
     </div>
   );
 }
---
+++ app/create-story/_components/TakePhoto.tsx
@@ -0,0 +1,86 @@
+import { Button } from "@nextui-org/button";
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@nextui-org/modal";
+import { Image } from "@nextui-org/react";
+import { useRef, useState } from "react";
+import { Camera } from "react-camera-pro";
+
+interface ITakePhoto {
+  isOpen: boolean;
+  onClose: () => void;
+  onOpenChange: () => void;
+  onPhotoPick: (image: string) => void;
+}
+
+export default function TakePhoto({
+  isOpen,
+  onClose,
+  onOpenChange,
+  onPhotoPick,
+}: ITakePhoto) {
+  const camera = useRef(null);
+  const [photo, setPhoto] = useState<string | null>(null);
+
+  const onTakePhoto = () => {
+    if (!camera.current) {
+      return;
+    }
+
+    const photo = camera.current.takePhoto();
+    setPhoto(photo);
+  };
+
+  const onRetakePhoto = () => {
+    setPhoto(null);
+  };
+
+  const onPickPhoto = () => {
+    onPhotoPick(photo!);
+    onClose();
+  };
+
+  return (
+    <Modal isOpen={isOpen} onOpenChange={onOpenChange} size="4xl">
+      <ModalContent>
+        <ModalHeader className="flex flex-col gap-1">Take Photo</ModalHeader>
+        <ModalBody className="h-96 min-h-96">
+          <div className="relative flex flex-1 w-full">
+            {!photo ? (
+              <Camera
+                ref={camera}
+                errorMessages={{
+                  noCameraAccessible:
+                    "No camera found. Please connect your camera.",
+                  permissionDenied:
+                    " You need to give permission to access the camera.",
+                  switchCamera: "Not possible to switch camera.",
+                  canvas: "Canvas not supported.",
+                }}
+                facingMode="environment"
+              />
+            ) : (
+              <img src={photo} width="100%" />
+            )}
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button onPress={onClose} color="danger">
+            Cancel
+          </Button>
+          {!photo && <Button onPress={onTakePhoto}>Take photo</Button>}
+          {photo && <Button onPress={onRetakePhoto}>Retake photo</Button>}
+          {photo && (
+            <Button onPress={onPickPhoto} color="primary">
+              Pick photo
+            </Button>
+          )}
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
